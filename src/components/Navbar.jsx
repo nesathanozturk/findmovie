40f9835166e44e06c9afdaa8409a6ad0ceb85179
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-26 22:06:02 +0300

    style: responsived navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <header>
-      <div className="w-full h-[70px] flex justify-between items-center py-8">
-        <h1 className="text-white text-3xl pl-10 font-semibold">
+      <div className="w-full h-[70px] flex flex-col items-center gap-2 py-8 lg:flex-row lg:justify-between lg:items-center">
+        <h1 className="text-white mr-8 text-2xl lg:text-3xl pl-10 font-semibold lg:mr-0">
           <Link to="/">
             Find<span className="text-orange-500">Movie</span>
           </Link>
@@ -12,10 +12,10 @@ function Navbar() {
         <input
           type="text"
           placeholder="Search a movie"
-          className="bg-transparent mt-2 p-2 rounded-lg outline-none bg-gray-700 text-white"
+          className="bg-transparent w-56 p-2 rounded-lg outline-none bg-gray-700 text-white lg:w-64 lg:mt-2"
         />
         <nav>
-          <ul className="flex px-10 text-orange-500 text-lg font-bold">
+          <ul className="flex px-10 text-orange-500 text-base lg:text-lg font-bold">
             <li className="mr-4 hover">
               <Link to="/">Home</Link>
             </li>
