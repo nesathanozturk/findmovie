14fca65ebee6be7676ddc5986a34b143d93e21c0
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-26 21:07:19 +0300

    feat: created routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies/:movieId" element={<Favorites />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
