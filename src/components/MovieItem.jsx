5449f7dd51bc63c1aa809a79835206102b9db54a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-27 11:44:33 +0300

    style: edited movies

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -2,11 +2,11 @@ function MovieItem({ movies }) {
   const renderedMovies = movies.map((movie) => (
     <div
       key={movie.imdbID}
-      className="bg-[#0d1d35] w-80 mb-1 relative overflow-hidden rounded-xl cursor-pointer text-center transition-transform hover:scale-105"
+      className="bg-[#1c2b4d] bg- w-80 mb-1 relative overflow-hidden rounded-xl cursor-pointer text-center transition-transform hover:scale-105"
     >
       <img className="w-full h-96" src={movie.Poster} alt={movie.Title} />
-      <div className="flex justify-center items-center flex-col gap-3 text-white text-lg py-[0.5rem] px-4">
-        <h3 className="pt-2">{movie.Title}</h3>
+      <div className="flex justify-center items-center flex-col gap-3 text-white text-xl font-semibold py-4 px-4">
+        <h3>{movie.Title}</h3>
         <span>{movie.Year}</span>
       </div>
     </div>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,7 +7,9 @@ function Movies() {
   return (
     <section className="py-4 pl-4">
       <div className="max-w-full m-auto">
-        <h1 className="text-white text-2xl ml-6 lg:text-3xl">Movies</h1>
+        <h1 className="text-white text-2xl ml-6 font-semibold lg:text-3xl">
+          Movies
+        </h1>
         <MovieItem movies={movies} />
       </div>
     </section>
