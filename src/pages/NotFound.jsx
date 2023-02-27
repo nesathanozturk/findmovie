189d2b4b8e0353bdc64fd64282e7345b23c371ca
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-27 12:03:46 +0300

    style: edited notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="w-full h-screen flex justify-center items-center flex-col text-white">
+      <h1 className="text-3xl">
+        We couldn&apos;t find the movie you looking for!
+      </h1>
+      <Link to="/" className="underline mt-4 text-xl">
+        Go back to the home page
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
