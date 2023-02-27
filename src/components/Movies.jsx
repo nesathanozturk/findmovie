e1c2c59bd1564aa35c2d6dbe2ca02a20544af968
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-27 11:30:49 +0300

    style: edited navbar

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,9 +2,9 @@ import MovieItem from "./MovieItem";
 
 function Movies({ movies }) {
   return (
-    <section className="py-8 pl-4">
+    <section className="py-4 pl-4">
       <div className="max-w-full m-auto">
-        <h1 className="text-white text-2xl mt-6 ml-6 lg:text-3xl">Movies</h1>
+        <h1 className="text-white text-2xl ml-6 lg:text-3xl">Movies</h1>
         <MovieItem movies={movies} />
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar({ searchValue, setSearchValue }) {
   return (
     <header>
-      <div className="w-full h-[80px] flex flex-col items-center gap-2 py-8 lg:flex-row lg:justify-between lg:items-center">
-        <h1 className="text-white mr-8 text-2xl lg:text-3xl pl-10 font-semibold lg:mr-0">
+      <div className="w-full flex flex-col items-center gap-2 py-8 lg:flex-row lg:justify-between">
+        <h1 className="text-white mr-8 text-3xl pl-10 font-semibold lg:mr-0">
           <Link to="/">
             Find<span className="text-orange-500">Movie</span>
           </Link>
@@ -14,10 +14,10 @@ function Navbar({ searchValue, setSearchValue }) {
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
           placeholder="Search a movie"
-          className="bg-transparent w-56 p-2 rounded-lg outline-none bg-gray-600 text-white lg:w-64 lg:mt-2"
+          className="bg-transparent w-56 p-2 rounded-lg outline-none bg-gray-500 text-white lg:w-64 lg:mt-2"
         />
         <nav>
-          <ul className="flex px-10 text-orange-500 text-base lg:text-lg font-bold">
+          <ul className="flex px-10 text-orange-500 text-lg font-bold">
             <li className="mr-4 hover">
               <Link to="/">Home</Link>
             </li>
