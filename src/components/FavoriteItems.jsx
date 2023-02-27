0fdb18102e6ccad75d33b99abc1972adb4d82915
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-27 14:33:33 +0300

    style: Edited favorites name

--- src/components/FavoriteItems.jsx
+++ src/components/FavoriteItems.jsx
@@ -7,13 +7,13 @@ function FavoriteItems() {
   return (
     <section className="p-6">
       <div className="max-w-full m-auto">
-        <h1 className="text-white text-2xl ml-6 font-semibold lg:text-3xl">
+        <h1 className="text-white text-2xl ml-4 font-semibold lg:text-3xl">
           Favorites
         </h1>
         {favorites.length > 0 ? (
           <FavoriteItem favorites={favorites} />
         ) : (
-          <p className="text-white text-lg mt-4 ml-6 lg:text-xl">
+          <p className="text-white text-lg mt-4 ml-4 lg:text-xl">
             You have no favorite movies!
           </p>
         )}
