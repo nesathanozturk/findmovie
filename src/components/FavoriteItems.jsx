80c31deddc5fc8d9283a24313c8bda77388966aa
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-02-27 15:39:22 +0300

    style: edited movies

--- src/components/FavoriteItems.jsx
+++ src/components/FavoriteItems.jsx
@@ -5,24 +5,22 @@ function FavoriteItems() {
   const { favorites, clearFavorites } = useMovieContext();
 
   return (
-    <section className="p-6">
+    <section className="p-6 mt-6">
       <div className="max-w-full m-auto">
         <div className="flex justify-between items-center">
-          <h1 className="text-white text-2xl ml-4 mb-8 font-semibold lg:text-3xl">
+          <h1 className="text-white text-2xl ml-4 mb-4 font-semibold lg:text-3xl">
             Favorites
           </h1>
-          {favorites.length > 0 ? (
+          {favorites.length > 1 ? (
             <button className="btn" onClick={() => clearFavorites(favorites)}>
               Clear Favorites
             </button>
           ) : null}
         </div>
         {favorites.length > 0 ? (
-          <>
-            <FavoriteItem favorites={favorites} />
-          </>
+          <FavoriteItem favorites={favorites} />
         ) : (
-          <p className="text-white text-lg mt-4 ml-4 lg:text-xl">
+          <p className="text-white text-lg ml-4 lg:text-xl">
             You have no favorite movies!
           </p>
         )}
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,11 @@
 import useMovieContext from "../hooks/use-movie-context";
 import MovieItem from "./MovieItem";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 
 function Movies() {
   const { movies } = useMovieContext();
 
   return (
-    <section className="p-6">
+    <section className="p-6 mt-6">
       <div className="max-w-full m-auto">
         <h1 className="text-white text-2xl ml-4 font-semibold lg:text-3xl">
           Movies
